bee41eb52d1e5e22523730a12fe2126b29625718
rongrongo0o0 <ryangxx@gmail.com>
2018-06-19 16:45:36 +0800

    BraveKnightTest

---
+++ chapter01/src/test/java/org/chapter01/sia/knights/BraveKnightTest.java
@@ -0,0 +1,26 @@
+package org.chapter01.sia.knights;
+
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+
+/**
+ * 勇敢的骑士测试类
+ * 
+ * @Title:BraveKnightTest.java
+ * @Package:org.chapter01.sia.knights
+ * @Description:
+ * @author:yangrong
+ * @date:2018年6月19日下午4:22:30
+ * @updater:
+ * @version:V1.0
+ * @updates:
+ */
+public class BraveKnightTest {
+	@Test
+	public void knightShouldEmbarkOnQuest() {
+		Quest mockQuest = mock(Quest.class);
+		BraveKnight knight = new BraveKnight(mockQuest);
+		knight.embarkOnQuest();
+		verify(mockQuest, times(1)).embark();// 验证embark()调用了一次
+	}
+}
