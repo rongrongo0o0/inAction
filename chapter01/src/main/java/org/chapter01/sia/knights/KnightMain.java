248ac5cdd5e897e9d38314b5eafddf286a440331
rongrongo0o0 <ryangxx@gmail.com>
2018-06-21 17:18:32 +0800

    spring xml suport execute programme

---
+++ chapter01/src/main/java/org/chapter01/sia/knights/KnightMain.java
@@ -0,0 +1,12 @@
+package org.chapter01.sia.knights;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class KnightMain {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("knight.xml");
+		Knight knight = context.getBean(Knight.class);
+		knight.embarkOnQuest();
+		context.close();
+	}
+}
